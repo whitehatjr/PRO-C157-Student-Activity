842ab41fcb35054701df80e940ac3777b7e8c7fa
whitehatjr <52084703+whitehatjr@users.noreply.github.com>
2021-01-04 19:48:21 +0530

    Update Tour.js

--- js/Tour.js
+++ js/Tour.js
@@ -39,60 +39,13 @@ AFRAME.registerComponent("tour", {
       prevoiusXPosition = posX;
 
       // Border Element
-      const borderEl = this.createBorder(position, item.id);
-
-      // Thubnail Element
-      const thumbNail = this.createThumbNail(item);
-      borderEl.appendChild(thumbNail);
-
+      
+      // Thumbnail Element
+     
       // Title Text Element
-      const titleEl = this.createTitleEl(position, item);
-      borderEl.appendChild(titleEl);
-
+      
       this.placesContainer.appendChild(borderEl);
     }
   },
-  createBorder: function (position, id) {
-    const entityEl = document.createElement("a-entity");
-    entityEl.setAttribute("id", id);
-    entityEl.setAttribute("visible", true);
-    entityEl.setAttribute("geometry", {
-      primitive: "ring",
-      radiusInner: 9,
-      radiusOuter: 10,
-    });
-    entityEl.setAttribute("position", position);
-    entityEl.setAttribute("material", {
-      color: "#0077CC",
-      opacity: 1,
-    });
-
-    return entityEl;
-  },
-  createThumbNail: function (item) {
-    const entityEl = document.createElement("a-entity");
-    entityEl.setAttribute("visible", true);
-    entityEl.setAttribute("geometry", {
-      primitive: "circle",
-      radius: 9,
-    });
-    entityEl.setAttribute("material", { src: item.url });
-
-    return entityEl;
-  },
-  createTitleEl: function (position, item) {
-    const entityEl = document.createElement("a-entity");
-    entityEl.setAttribute("text", {
-      font: "exo2bold",
-      align: "center",
-      width: 70,
-      color: "#e65100",
-      value: item.title,
-    });
-    const elPosition = position;
-    elPosition.y = -20;
-    entityEl.setAttribute("position", elPosition);
-    entityEl.setAttribute("visible", true);
-    return entityEl;
-  },
+  
 });
